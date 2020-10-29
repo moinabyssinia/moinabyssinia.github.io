badee77191aba012c0b5c7c976cb1241ab350663
moinabyssinia <michaelg.tadesse@gmail.com>
2020-10-28 22:13:58 -0400

    sticky navbar

---
+++ projects/project.js
@@ -0,0 +1,16 @@
+const nav = document.querySelector(".wrapper");
+
+const topNav = nav.offsetTop;
+
+function fixNav(){
+    console.log(topNav);
+    if(window.scrollY > topNav){
+      document.body.style.paddingTop = nav.offsetHeight + 'px';
+      document.body.classList.add('fixed-nav');
+    } else {
+      document.body.style.paddingTop = 0;
+      document.body.classList.remove('fixed-nav');
+    }
+ }
+
+ window.addEventListener('scroll', fixNav);
